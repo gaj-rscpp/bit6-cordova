252b5ed1a027f37b313dd2272c690ca50b400c68
Gaj Mohan <gaj@rscpp.co.uk>
2016-05-11 10:38:09 +0100

    Update PhoneRTCPlugin.java

--- src/android/com/dooble/phonertc/PhoneRTCPlugin.java
+++ src/android/com/dooble/phonertc/PhoneRTCPlugin.java
@@ -276,8 +276,8 @@ public class PhoneRTCPlugin extends CordovaPlugin {
 	        return null;
 	    }
 
-	    if (webView instanceof WebView ) {
-	        _webView = (WebView) webView;
+	    if (CordovaWebView instanceof WebView ) {
+	        _webView = (WebView) CordovaWebView;
 	    }
 	    else { //(>=4.0.0), using getView method
 	        java.lang.reflect.Method method = null;
